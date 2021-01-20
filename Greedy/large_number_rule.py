726d3bfe30d6fd12b46456e868ecd6cd34ac0ed0
Wonjin Lee <claude17@naver.com>
2021-01-21 00:20:02 +0900

    [그리디] 숫자 카드 게임

--- Greedy/large_number_rule.py
+++ Greedy/large_number_rule.py
@@ -20,10 +20,29 @@ def sum_large_number(number_list, m, k):
         if m == 0:
             return total
 
+def sum_large_number_enhanced(number_list, m, k):
+    total = 0
+
+    number_list = sorted(number_list, reverse=True)
+    first_number = number_list[0]
+    second_number = number_list[1]
+
+    # 반복되는 패턴 : k + 1
+    # 완전한 패턴이 몇 번이나 반복되는가? : m // (k + 1)
+    # 정확히 나누어 떨어지지 않는 경우는? : (m % (k + 1)) * first_number
+
+    repetition = m // (k + 1)
+
+    total += (m - repetition) * first_number + repetition * second_number
+    total += (m % (k + 1)) * first_number
+
+    return total
+
 number_list = [2, 4, 5, 4, 6]
 print(sum_large_number(number_list, 8, 3))
+print(sum_large_number_enhanced(number_list, 8, 3))
 
 # N, M, K를 공백으로 구분하여 입력받기
-n, m, k = map(int, input().split())
+# n, m, k = map(int, input().split())
 # N개의 수를 공백으로 구분하여 입력받기
-data = list(map(int, input().split()))
+# data = list(map(int, input().split()))
---
+++ Greedy/number_card_game.py
@@ -0,0 +1,19 @@
+def best_card(card_board):
+    best_card_number = 0
+    for i in range(len(card_board)):
+        min_number_in_row = sorted(card_board[i])[0]
+        if min_number_in_row > best_card_number:
+            best_card_number = min_number_in_row
+
+    return best_card_number
+
+# card_board = [[3, 1, 2], [4, 1, 4], [2, 2, 2]]
+card_board = [[7, 3, 1, 8], [3, 3, 3, 4]]
+print(best_card(card_board))
+
+'''
+    min(), max() 함수를 사용하는 방법을 사용했다면 숏코딩을 할 수 있었을 것이다.
+    ex) min_number_in_row = min(card_board[i])
+        best_card_number = max(min_number_in_row, best_card_number)
+'''
+
