c59ec1c74baa55e346c4828b9ce5d6012dc3d3a4
Wonjin Lee <claude17@naver.com>
2021-01-26 22:54:23 +0900

    [구현] 시각

---
+++ Implementation/time.py
@@ -0,0 +1,13 @@
+def solution(n):
+
+    count = 0
+
+    for hour in range(n + 1):
+        for minute in range(60):
+            for second in range(60):
+                if '3' in str(hour) + str(minute) + str(second):
+                    count += 1
+
+    return count
+
+print(solution(5))
