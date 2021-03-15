46654178696ef98c5f40a23c38adb77ac51f2756
Wonjin Lee <claude17@naver.com>
2021-03-15 23:44:01 +0900

    [DP] Top-Down & Bottom-Up 방식의 피보나치 수열 코드

--- DynamicProgramming/fibonacci_numbers.py
+++ DynamicProgramming/fibonacci_numbers.py
@@ -1,5 +1,6 @@
 memo = [0] * 100
 
+# Top-Down
 def fibo(x):
     if x == 1 or x == 2:
         return 1
@@ -11,3 +12,13 @@ def fibo(x):
     return memo[x]
 
 print(fibo(99))
+
+# Bottom-Up
+memo[1] = 1
+memo[2] = 1
+n = 99
+
+for i in range(3, n + 1):
+    memo[i] = memo[i - 1] + memo[i - 2]
+
+print(memo[n])
