cedcf9c2adbab5010445a8eb627bedb49ddf0323
Wonjin Lee <claude17@naver.com>
2021-08-23 23:55:14 +0900

    [DP] 1로 만들기 - Memoization

---
+++ DynamicProgramming/make_one.py
@@ -0,0 +1,18 @@
+def solution(num):
+
+    # Bottom-Up
+    for i in range(2, num + 1):
+        dp[i] = dp[i - 1] + 1
+
+        if i % 2 == 0:
+            dp[i] = min(dp[i], dp[i // 2] + 1)
+        if i % 3 == 0:
+            dp[i] = min(dp[i], dp[i // 3] + 1)
+        if i % 5 == 0:
+            dp[i] = min(dp[i], dp[i // 5] + 1)
+
+    return dp[num]
+
+dp = [0] * 30001
+
+print(solution(26))
